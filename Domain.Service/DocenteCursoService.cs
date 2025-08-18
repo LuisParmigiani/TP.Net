524dcae79f63bd2872cb6721b44b9b8e485593d7
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-08-18 19:32:50 -0300

    Fix DocenteCursoService syntax and add DocenteCurso.cs

--- Domain.Service/DocenteCursoService.cs
+++ Domain.Service/DocenteCursoService.cs
@@ -10,53 +10,49 @@ namespace Domain.Service
 {
     public class DocenteCursoService
     {
-            public void Add(DocenteCurso docenteCurso)
-            {
+        public void Add(DocenteCurso docenteCurso)
+        {
             DocenteCursoInMemory.DocenteCursos.Add(docenteCurso);
-            }
+        }
 
-
-            public bool Delete(int id)
-            {
+        public bool Delete(int id)
+        {
             DocenteCurso? docenteCursoToDelete = DocenteCursoInMemory.DocenteCursos.Find(x => x.Id == id);
-                if (docenteCursoToDelete != null)
-                {
+            if (docenteCursoToDelete != null)
+            {
                 DocenteCursoInMemory.DocenteCursos.Remove(docenteCursoToDelete);
-                    return true;
-                }
+                return true;
+            }
             else
             {
-                                        
-            }
-                {
-                    return false;
-                }
-            }
-
-            public DocenteCurso Get(int id)
-            {
-                return DocenteCursoInMemory.DocenteCursos.FirstOrDefault(x => x.Id == id);
-            }
-
-            public IEnumerable<DocenteCurso> GetAll()
-            {
-                return DocenteCursoInMemory.DocenteCursos.ToList();
-            }
-
-            public bool Update(DocenteCurso docenteCurso)
-            {
-                var docenteCursoToUpdate = DocenteCursoInMemory.DocenteCursos.FirstOrDefault(x => x.Id == docenteCurso.Id);
-                if (docenteCursoToUpdate != null)
-                {
-                    docenteCursoToUpdate.SetCargo(docenteCurso.Cargo);
-                    docenteCursoToUpdate.SetIDCurso(docenteCurso.IDCurso);
-                    docenteCursoToUpdate.SetIDDocente(docenteCurso.IDDocente);
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
         }
+
+        public DocenteCurso Get(int id)
+        {
+            return DocenteCursoInMemory.DocenteCursos.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<DocenteCurso> GetAll()
+        {
+            return DocenteCursoInMemory.DocenteCursos.ToList();
+        }
+
+        public bool Update(DocenteCurso docenteCurso)
+        {
+            var docenteCursoToUpdate = DocenteCursoInMemory.DocenteCursos.FirstOrDefault(x => x.Id == docenteCurso.Id);
+            if (docenteCursoToUpdate != null)
+            {
+                docenteCursoToUpdate.SetCargo(docenteCurso.Cargo);
+                docenteCursoToUpdate.SetIDCurso(docenteCurso.IDCurso);
+                docenteCursoToUpdate.SetIDDocente(docenteCurso.IDDocente);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 }
--- Intnto 111111/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ Intnto 111111/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64fd53edee9711921f24160e3c7ca39fd02cbacb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577c091c6955d58864337ba48b48c882e7a0b245")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
