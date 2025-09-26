d36a6e5874930564f113c04184dd6a066cd8cbba
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-09-26 19:17:44 -0300

    porfin

---
+++ WebApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using WebApp.Components;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("API", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5183"); // <-- usar HTTP
+});
+
+
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
